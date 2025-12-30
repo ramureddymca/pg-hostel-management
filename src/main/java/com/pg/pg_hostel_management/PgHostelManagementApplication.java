6a087325c6daae981f31e3c0e472399acee88d4c
ramureddymca <ramureddymca@gmail.com>
2025-12-31 02:34:09 +0800

    Initial Spring Boot setup for PG/Hostel Management

---
+++ src/main/java/com/pg/pg_hostel_management/PgHostelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pg.pg_hostel_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgHostelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgHostelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pg/pg_hostel_management/PgHostelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.pg_hostel_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgHostelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
